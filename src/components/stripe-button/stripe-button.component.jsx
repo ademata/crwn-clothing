bb46f84f6b1d5e97427bb6baf7fa044c577cb091
ademata <aldemata1@gmail.com>
2019-12-06 07:21:56 +0800

    modified firebase and stripe configuration

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,8 +3,8 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
-
+  //const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_QrnQkopp3HVDG3QdoNapxaq200TxunBVjP';
   const onToken = token => {
     console.log(token);
     alert('Payment Succesful!');
